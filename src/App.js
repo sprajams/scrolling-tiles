859508bc2f678371ab3c9ce85e1a7ae9ac457aae
Suphasuta Prajamsri <suphprajam95@gmail.com>
2022-07-07 20:43:29 -0700

    animate scroll, no repeats

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
+import clsx from "clsx";
 import TilesContainer from "./components/TilesContainer";
 import "./App.css";
 
 function App() {
   const [imgSrc, setImgSrc] = useState("");
+  const [xVal, setXVal] = useState(0);
+
+  const requestRef = useRef();
+
+  const animate = (time) => {
+    setXVal((prev) => prev + 1);
+    requestRef.current = requestAnimationFrame(animate);
+  };
+
+  useEffect(() => {
+    requestRef.current = requestAnimationFrame(animate);
+    return () => {
+      cancelAnimationFrame(requestRef.current);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [xVal]);
+
   return (
     <div className="outer">
-      <TilesContainer setImgSrc={setImgSrc} />
+      <div style={{ transform: `translateX(-${xVal}px)` }}>
+        <TilesContainer setImgSrc={setImgSrc} />
+      </div>
       {imgSrc.length > 0 ? (
         <div className="imgWrap--large">
           <img className="img--large" src={imgSrc} alt="cute animal" />
