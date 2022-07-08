a444afa1b8ee929d4cbd0d98ace8e7893017d447
Suphasuta Prajamsri <suphprajam95@gmail.com>
2022-07-08 15:37:45 -0700

    functioning infinite scroll of puppies

--- src/App.js
+++ src/App.js
@@ -4,27 +4,28 @@ import Clone from "./components/Clone";
 import "./App.css";
 
 function App() {
-  const HEIGHT = 100;
-  const WIDTH = 100;
-  const SPEED = 0.8;
-
   const [imgSrc, setImgSrc] = useState("");
-  const [xVal, setXVal] = useState(0);
 
-  const requestRef = useRef();
+  // const HEIGHT = 100;
+  // const WIDTH = 100;
+  // const SPEED = 0.8;
 
-  const animate = () => {
-    setXVal((curr) => (curr + 1 * SPEED) % WIDTH);
-    requestRef.current = requestAnimationFrame(animate);
-  };
+  // const [xVal, setXVal] = useState(0);
 
-  useEffect(() => {
-    requestRef.current = requestAnimationFrame(animate);
-    return () => {
-      cancelAnimationFrame(requestRef.current);
-    };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [xVal]);
+  // const requestRef = useRef();
+
+  // const animate = () => {
+  //   setXVal((curr) => (curr + 1 * SPEED) % WIDTH);
+  //   requestRef.current = requestAnimationFrame(animate);
+  // };
+
+  // useEffect(() => {
+  //   requestRef.current = requestAnimationFrame(animate);
+  //   return () => {
+  //     cancelAnimationFrame(requestRef.current);
+  //   };
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, [xVal]);
 
   return (
     <div className="outer">
--- src/components/TilesContainer/index.jsx
+++ src/components/TilesContainer/index.jsx
@@ -1,3 +1,4 @@
+import { useCallback } from "react";
 import { useState, useRef, useEffect } from "react";
 import Tile from "../Tile";
 import styles from "./styles.module.scss";
@@ -7,13 +8,13 @@ const Puppies = [
   "https://frontendeval.com/images/puppy-3.jpeg",
   "https://frontendeval.com/images/puppy-4.jpeg",
   "https://frontendeval.com/images/puppy-5.jpeg",
-  // "https://frontendeval.com/images/puppy-6.jpeg",
-  // "https://frontendeval.com/images/puppy-7.jpeg",
-  // "https://frontendeval.com/images/puppy-8.jpeg",
-  // "https://frontendeval.com/images/puppy-9.jpeg",
-  // "https://frontendeval.com/images/puppy-10.jpeg",
-  // "https://frontendeval.com/images/puppy-11.jpeg",
-  // "https://frontendeval.com/images/puppy-12.jpeg",
+  "https://frontendeval.com/images/puppy-6.jpeg",
+  "https://frontendeval.com/images/puppy-7.jpeg",
+  "https://frontendeval.com/images/puppy-8.jpeg",
+  "https://frontendeval.com/images/puppy-9.jpeg",
+  "https://frontendeval.com/images/puppy-10.jpeg",
+  "https://frontendeval.com/images/puppy-11.jpeg",
+  "https://frontendeval.com/images/puppy-12.jpeg",
 ];
 
 const Kittens = [
@@ -38,28 +39,30 @@ function TilesContainer({ setImgSrc }) {
   };
 
   const [xVal, setXVal] = useState(0);
+  const [baseSpeed, setBaseSpeed] = useState(3);
   const requestRef = useRef(null);
   const widthRef = useRef(null);
 
-  const SPEED = 5;
   const [listWidth, setListWidth] = useState(0);
-  useEffect(() => {
-    setListWidth(widthRef.current ? widthRef.current.offsetWidth : 0);
-  }, []);
 
-  // const WIDTH = console.log(WIDTH);
-  const animate = () => {
-    // setXVal((curr) => (listWidth > 0 ? (curr + 1 * SPEED) % listWidth : 0));
+  //setXVal will only change if listWidth or baseSpeed is changed
+  const animate = useCallback(() => {
+    // adjust xVal every animation frame, % listWidth helps reset xVal back to 0
+    setXVal((curr) => (listWidth > 0 ? (curr + 1 * baseSpeed) % listWidth : 0));
     requestRef.current = requestAnimationFrame(animate);
-  };
+  }, [listWidth, baseSpeed]);
 
   useEffect(() => {
+    // get width of 1 list of animals
+    const width = widthRef.current ? widthRef.current.offsetWidth : 0;
+    setListWidth(width);
+
     requestRef.current = requestAnimationFrame(animate);
     return () => {
       cancelAnimationFrame(requestRef.current);
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [xVal]);
+  }, [animate]);
 
   return (
     <div>
@@ -67,11 +70,17 @@ function TilesContainer({ setImgSrc }) {
         <ul
           className={styles.animalList}
           ref={widthRef}
-          style={{ transform: `translate(${-xVal}px)` }}
+          style={{
+            transform: `translate(${-xVal}px)`,
+          }}
         >
           {Puppies.map((x, i) => {
             return (
-              <li key={"dog-" + i} onClick={onClick}>
+              <li
+                key={"dog-" + i}
+                onClick={onClick}
+                className={styles.animalListItem}
+              >
                 <Tile src={x} />
               </li>
             );
@@ -79,8 +88,9 @@ function TilesContainer({ setImgSrc }) {
         </ul>
         <ul
           className={styles.animalList}
+          // +100% adds on the width of its parent, this keeps the 2nd ul right behind 1st ul
           style={{
-            transform: `translate(${-xVal + listWidth}px)`,
+            transform: `translate(calc(${-xVal}px + 100% + 10px))`,
           }}
         >
           {Puppies.map((x, i) => {
