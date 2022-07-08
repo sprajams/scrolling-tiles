465e02c09c5af4fe2ea8f9d553f5cf87f2116919
Suphasuta Prajamsri <suphprajam95@gmail.com>
2022-07-08 16:13:47 -0700

    code clean up

--- src/App.js
+++ src/App.js
@@ -1,45 +1,18 @@
-import { useState, useRef, useEffect } from "react";
+import { useState } from "react";
 import TilesContainer from "./components/TilesContainer";
-import Clone from "./components/Clone";
 import "./App.css";
 
 function App() {
   const [imgSrc, setImgSrc] = useState("");
 
-  // const HEIGHT = 100;
-  // const WIDTH = 100;
-  // const SPEED = 0.8;
-
-  // const [xVal, setXVal] = useState(0);
-
-  // const requestRef = useRef();
-
-  // const animate = () => {
-  //   setXVal((curr) => (curr + 1 * SPEED) % WIDTH);
-  //   requestRef.current = requestAnimationFrame(animate);
-  // };
-
-  // useEffect(() => {
-  //   requestRef.current = requestAnimationFrame(animate);
-  //   return () => {
-  //     cancelAnimationFrame(requestRef.current);
-  //   };
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, [xVal]);
-
   return (
     <div className="outer">
-      {/* <div style={{ transform: `translateX(-${xVal}px)` }}> */}
       <TilesContainer setImgSrc={setImgSrc} />
-      {/* </div> */}
       {imgSrc.length > 0 ? (
         <div className="imgWrap--large">
           <img className="img--large" src={imgSrc} alt="cute animal" />
         </div>
       ) : null}
-      {/* <div>
-        <Clone />
-      </div> */}
     </div>
   );
 }
