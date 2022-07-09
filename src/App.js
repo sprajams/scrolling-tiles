0e75efb44f314e4cc2954f1125809303ded17001
Suphasuta Prajamsri <suphprajam95@gmail.com>
2022-07-08 17:14:56 -0700

    refactor to make scrolling resuable, pause btn

--- src/App.js
+++ src/App.js
@@ -1,13 +1,61 @@
 import { useState } from "react";
-import TilesContainer from "./components/TilesContainer";
+import ScrollingTiles from "./components/ScrollingTiles";
 import "./App.css";
+
+const PUPPIES = [
+  "https://frontendeval.com/images/puppy-1.jpeg",
+  "https://frontendeval.com/images/puppy-2.jpeg",
+  "https://frontendeval.com/images/puppy-3.jpeg",
+  "https://frontendeval.com/images/puppy-4.jpeg",
+  "https://frontendeval.com/images/puppy-5.jpeg",
+  "https://frontendeval.com/images/puppy-6.jpeg",
+  "https://frontendeval.com/images/puppy-7.jpeg",
+  "https://frontendeval.com/images/puppy-8.jpeg",
+  "https://frontendeval.com/images/puppy-9.jpeg",
+  "https://frontendeval.com/images/puppy-10.jpeg",
+  "https://frontendeval.com/images/puppy-11.jpeg",
+  "https://frontendeval.com/images/puppy-12.jpeg",
+];
+
+const KITTENS = [
+  "https://frontendeval.com/images/kitten-1.jpeg",
+  "https://frontendeval.com/images/kitten-2.jpeg",
+  "https://frontendeval.com/images/kitten-3.jpeg",
+  "https://frontendeval.com/images/kitten-4.jpeg",
+  "https://frontendeval.com/images/kitten-5.jpeg",
+  "https://frontendeval.com/images/kitten-6.jpeg",
+  "https://frontendeval.com/images/kitten-7.jpeg",
+  "https://frontendeval.com/images/kitten-8.jpeg",
+  "https://frontendeval.com/images/kitten-9.jpeg",
+  "https://frontendeval.com/images/kitten-10.jpeg",
+  "https://frontendeval.com/images/kitten-11.jpeg",
+  "https://frontendeval.com/images/kitten-12.jpeg",
+];
 
 function App() {
   const [imgSrc, setImgSrc] = useState("");
-
+  const [baseSpeed, setBaseSpeed] = useState(3);
+  const [isPaused, setIsPaused] = useState(false);
+  const onClick = () => {
+    setIsPaused(!isPaused);
+  };
   return (
     <div className="outer">
-      <TilesContainer setImgSrc={setImgSrc} />
+      <div>
+        <button onClick={onClick} className="btn">
+          {isPaused ? `play` : `paws`}
+        </button>
+        <ScrollingTiles
+          setImgSrc={setImgSrc}
+          petImgArr={PUPPIES}
+          baseSpeed={isPaused ? 0 : baseSpeed}
+        />
+        <ScrollingTiles
+          setImgSrc={setImgSrc}
+          petImgArr={KITTENS}
+          baseSpeed={isPaused ? 0 : baseSpeed + 1}
+        />
+      </div>
       {imgSrc.length > 0 ? (
         <div className="imgWrap--large">
           <img className="img--large" src={imgSrc} alt="cute animal" />
--- src/components/ScrollingTiles/index.jsx
+++ src/components/ScrollingTiles/index.jsx
@@ -2,44 +2,14 @@ import { useCallback } from "react";
 import { useState, useRef, useEffect } from "react";
 import Tile from "../Tile";
 import styles from "./styles.module.scss";
-const Puppies = [
-  "https://frontendeval.com/images/puppy-1.jpeg",
-  "https://frontendeval.com/images/puppy-2.jpeg",
-  "https://frontendeval.com/images/puppy-3.jpeg",
-  "https://frontendeval.com/images/puppy-4.jpeg",
-  "https://frontendeval.com/images/puppy-5.jpeg",
-  "https://frontendeval.com/images/puppy-6.jpeg",
-  "https://frontendeval.com/images/puppy-7.jpeg",
-  "https://frontendeval.com/images/puppy-8.jpeg",
-  "https://frontendeval.com/images/puppy-9.jpeg",
-  "https://frontendeval.com/images/puppy-10.jpeg",
-  "https://frontendeval.com/images/puppy-11.jpeg",
-  "https://frontendeval.com/images/puppy-12.jpeg",
-];
 
-const Kittens = [
-  "https://frontendeval.com/images/kitten-1.jpeg",
-  "https://frontendeval.com/images/kitten-2.jpeg",
-  "https://frontendeval.com/images/kitten-3.jpeg",
-  "https://frontendeval.com/images/kitten-4.jpeg",
-  "https://frontendeval.com/images/kitten-5.jpeg",
-  "https://frontendeval.com/images/kitten-6.jpeg",
-  "https://frontendeval.com/images/kitten-7.jpeg",
-  "https://frontendeval.com/images/kitten-8.jpeg",
-  "https://frontendeval.com/images/kitten-9.jpeg",
-  "https://frontendeval.com/images/kitten-10.jpeg",
-  "https://frontendeval.com/images/kitten-11.jpeg",
-  "https://frontendeval.com/images/kitten-12.jpeg",
-];
-
-function TilesContainer({ setImgSrc }) {
+function ScrollingTiles({ setImgSrc, petImgArr, baseSpeed }) {
   //  clicking on an image tile will store the src and display it in App.js
   const onClick = (e) => {
     setImgSrc(e.target.src);
   };
 
   const [xVal, setXVal] = useState(0);
-  const [baseSpeed, setBaseSpeed] = useState(3);
   const requestRef = useRef(null);
   const widthRef = useRef(null);
 
@@ -56,7 +26,6 @@ function TilesContainer({ setImgSrc }) {
     // get width of 1 list of animals
     const width = widthRef.current ? widthRef.current.offsetWidth : 0;
     setListWidth(width);
-
     requestRef.current = requestAnimationFrame(animate);
     return () => {
       cancelAnimationFrame(requestRef.current);
@@ -74,7 +43,7 @@ function TilesContainer({ setImgSrc }) {
             transform: `translate(${-xVal}px)`,
           }}
         >
-          {Puppies.map((x, i) => {
+          {petImgArr.map((x, i) => {
             return (
               <li
                 key={"dog-" + i}
@@ -93,7 +62,7 @@ function TilesContainer({ setImgSrc }) {
             transform: `translate(calc(${-xVal}px + 100% + 10px))`,
           }}
         >
-          {Puppies.map((x, i) => {
+          {petImgArr.map((x, i) => {
             return (
               <li key={"dog-" + i} onClick={onClick}>
                 <Tile src={x} />
@@ -102,18 +71,7 @@ function TilesContainer({ setImgSrc }) {
           })}
         </ul>
       </div>
-      {/* <div>
-        <ul className={styles.animalList}>
-          {Kittens.map((x, i) => {
-            return (
-              <li key={`kitten-` + i} onClick={onClick}>
-                <Tile src={x} />
-              </li>
-            );
-          })}
-        </ul>
-      </div> */}
     </div>
   );
 }
-export default TilesContainer;
+export default ScrollingTiles;
